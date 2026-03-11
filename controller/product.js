1a3fce024342053aa2dc9a992d903faa31bc602d
sushmabhapkar <sushmabhapkar244@gmail.com>
2026-03-11 13:59:25 +0530

    improvments made

--- controller/product.js
+++ controller/product.js
@@ -3,6 +3,8 @@ const sequelize = require("../util/db");
 const awsService = require("../util/AWSUploads");
 const sharp = require("sharp");
 const ALLOWED_UNITS = ["pcs", "jodi", "dozen"];
+const path = require("path");
+
 
 //Add a product
 // exports.addProduct = async (req, res) => {
@@ -429,6 +431,17 @@ exports.updateDefaultUnit = async (req, res) => {
 
 
 exports.downloadStockExcel = (req, res) => {
-  const filePath = "./stock-maximization-report.xlsx";
-  res.download(filePath);
+  const filePath = path.join(__dirname, "../stock-maximization-report.xlsx");
+
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  );
+
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=stock-maximization-report.xlsx"
+  );
+
+  res.sendFile(filePath);
 };
